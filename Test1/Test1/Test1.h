5004ffe36348600f6ffa7de7627001c1fd410b8e
Matthew O'Leary <moleary@zendesk.com>
2020-04-21 10:39:57 +0100

    Initial commit

---
+++ Test1/Test1/Test1.h
@@ -0,0 +1,19 @@
+//
+//  Test1.h
+//  Test1
+//
+//  Created by Matthew O'Leary on 21/04/2020.
+//  Copyright © 2020 Matthew O'Leary. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Test1.
+FOUNDATION_EXPORT double Test1VersionNumber;
+
+//! Project version string for Test1.
+FOUNDATION_EXPORT const unsigned char Test1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test1/PublicHeader.h>
+
+
---
+++ Test1/Test1/TestClass1.swift
@@ -0,0 +1,9 @@
+//
+//  TestClass1.swift
+//  Test1
+//
+//  Created by Matthew O'Leary on 21/04/2020.
+//  Copyright © 2020 Matthew O'Leary. All rights reserved.
+//
+
+import Foundation
